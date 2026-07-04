b55020e7d1de62447f26e1ce240101cfba203eb3
Jeff Davis <hoyajavis@gmail.com>
2026-07-04 10:40:57 +0900

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/math-mutiny/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
